bf1bea1a3fabbd8e94afb796c7c17a9ebe2e6915
Michael Steuer <michael@make.software>
2025-05-06 17:00:57 -0700

    debug
    Signed-off-by: Michael Steuer <michael@make.software>

--- backfill.js
+++ backfill.js
@@ -37,8 +37,8 @@ async function main() {
                     // checking if delegator owns NFT
                     const nft_url = `${process.env.CSPR_CLOUD_REST_URL}/accounts/${delegation.public_key}/nft-token-ownership?contract_package_hash=${process.env.NFT_CONTRACT_PACKAGE_HASH.slice(5)}&includes=owner_public_key`;
                     fetch(nft_url, { headers: headers }).then(res => res.json())
-                        console.log(data);
                         .then(data => {
+                            console.log(data);
                             if(data.data.length === 0) {
                                 console.log("Backfilling NFT to: " + delegation.public_key);
                                 delegationReceived(delegation.public_key, (BigInt(delegation.stake) / BigInt(1000000000)).toLocaleString('en-US', {}))
